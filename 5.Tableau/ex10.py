1272bc005d51bd1d4d9e99357bb1efc5794970cf
julien Lacroix <102021078+J7lmo@users.noreply.github.com>
2023-06-12 13:16:36 +0200

    Update ex10.py

--- 5.Tableau/ex10.py
+++ 5.Tableau/ex10.py
@@ -1,2 +1,2 @@
 #Écrivez une fonction qui prend en paramètre un tableau de nombres et le trie par ordre croissant puis retourne le tableau trié
-#Interdiction d'utiliser la fonction sorted
+#Interdiction d'utiliser la fonction sorted()
